e8e82f145b53a2c1b0e347227e037dfdafd59a69
e-boot <ebootinbox@gmail.com>
2024-09-06 18:18:30 +0100

    fixed router

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-import { renderPage, route } from './router.js';
-import { createBanner } from './views/components/banner.js';
+import { renderPage } from './router.js';
 
-/**
- * Event listener for when the initial HTML document has been completely loaded and parsed.
- * 
- * @param {Event} event - The DOMContentLoaded event
- */
-window.addEventListener("DOMContentLoaded", () => {
-    // Render the correct page on initial load
-    createBanner();
-    renderPage(window.location.pathname);
+document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', function (event) {
+        event.preventDefault();
 
-    // Attach the route event to all 
-    document.querySelectorAll('button').forEach(anchor => {
-        anchor.addEventListener('click', route());
+        const path = event.target.getAttribute('href');
+
+        window.history.pushState({},'',path);
+
+        renderPage(path);
     });
+    
 });
 
+window.addEventListener('popstate', () => {
+    renderPage(document.location.pathname);
+});
+
+renderPage(document.location.pathname);
--- js/router.js
+++ js/router.js
@@ -2,30 +2,21 @@ import { createMainView } from "./views/mainView.js";
 import { createAppearances } from "./views/appearancesView.js";
 import { createBio } from "./views/bioView.js";
 
-const routes = {
-    '/': createMainView,
-    '/index.html': createMainView,
-    '/biography': createBio,
-    '/appearances': createAppearances
-};
+const routes = [
+    {path: '/', page: createMainView},
+    {path: '/index',page: createMainView},
+    { path: '/biography', page: createBio},
+    { path:'/appearances', page: createAppearances}
+]
 
+// Function to find route and render a page
 export function renderPage(path) {
-    const page = routes[path] || createMainView;
-    page();
+    // Find the correct route given the path
+    const route = routes.find(r => r.path === path);
+
+    if (route) {
+        route.page();
+    } else {
+        createMainView();
+    }
 }
-
-export function route(event) {
-    event.preventDefault();
-    const path = event.target.getAttribute('href');
-    
-    // Update the browser URL without reloading the page
-    window.history.pushState({}, '', path);
-
-    // Render the new page
-    renderPage(path);
-}
-
-// Handle browser back/forward navigation
-window.addEventListener('popstate', () => {
-    renderPage(window.location.pathname);
-});
